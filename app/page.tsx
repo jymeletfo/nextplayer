fe8769cf2139b8f97a5a0378738e562675b00d1a
TFO\jymele <jymele@tfo.org>
2024-12-11 16:56:19 -0500

    Fixed issues

--- app/page.tsx
+++ app/page.tsx
@@ -8,51 +8,6 @@ type VideoThumbnailProps = {
   duration: string;
 };
 
-const videos: VideoThumbnailProps[] = [
-  {
-    thumbnailUrl: "/placeholder.svg",
-    title: "Video 1",
-    description: "Description for Video 1",
-    duration: "2:30",
-  },
-  {
-    thumbnailUrl: "/placeholder.svg",
-    title: "Video 2",
-    description: "Description for Video 2",
-    duration: "1:45",
-  },
-  {
-    thumbnailUrl: "/placeholder.svg",
-    title: "Video 3",
-    description: "Description for Video 3",
-    duration: "3:15",
-  },
-  {
-    thumbnailUrl: "/placeholder.svg",
-    title: "Video 4",
-    description: "Description for Video 4",
-    duration: "2:05",
-  },
-  {
-    thumbnailUrl: "/placeholder.svg",
-    title: "Video 5",
-    description: "Description for Video 5",
-    duration: "3:45",
-  },
-  {
-    thumbnailUrl: "/placeholder.svg",
-    title: "Video 6",
-    description: "Description for Video 6",
-    duration: "4:20",
-  },
-  {
-    thumbnailUrl: "/placeholder.svg",
-    title: "Video 7",
-    description: "Description for Video 7",
-    duration: "5:00",
-  },
-];
-
 export default async function Home() {
   const products = await getProducts();
 
